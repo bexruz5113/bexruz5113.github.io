97a5158ab1337899376db26ac11d77e2a02c87fc
bexruz5113 <bexruzbahronov2000@gmail.com>
2021-01-11 22:37:31 +0500

    New Edustage

---
+++ JS/main.js
@@ -0,0 +1,48 @@
+$('.carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverpause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+$('.carousel-slide').owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverpause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 2
+        }
+    }
+})
+// 
+var elSiteNav = document.querySelector('.siteNav');
+
+document.addEventListener('scroll',function(){
+            if(window.scrollY>500){
+                elSiteNav.classList.add('siteNav-fixed');
+                document.body.style.marginTop=elSiteNav.scrollHeight+ 'px';
+            }
+            else{
+                elSiteNav.classList.remove('siteNav-fixed');
+                document.body.style.marginTop='0';
+            }
+})
+ 
